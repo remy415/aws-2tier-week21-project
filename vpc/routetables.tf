ffb184e61314489abb7dfd923b1fd360cc5ff33d
Kinsey Parham <kinseyparham@gmail.com>
2022-08-25 22:55:30 +0000

    commit

---
+++ vpc/routetables.tf
@@ -0,0 +1,26 @@
+resource "aws_route_table_association" "public_assoc" {
+  count = var.public_sn_count
+  subnet_id = aws_subnet.public_subnet.*.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.KP21_vpc.id
+
+  tags = {
+    Name = "public_rt"
+  }
+
+}
+resource "aws_route" "default_route" {
+  route_table_id = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.KP21_vpc.id
+
+  tags = {
+    Name = "private_rt"
+  }
+}
